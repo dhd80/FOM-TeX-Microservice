fab67e8340405c1ccaa778a56eb62e8d19ab7af6
Dennis Hodapp <dennis.hodapp@smartinsurtech.de>
2022-03-01 20:14:43 +0100

    cleanup

--- app/watchfile.sh
+++ app/watchfile.sh
@@ -9,15 +9,15 @@ LTIME=`stat -c %Z /usr/fomtextemplate/deine_inhalte/Kapitel.tex`
 
 while true    
 do
-   	ATIME=`stat -c %Z /usr/fomtextemplate/deine_inhalte/Kapitel.tex`
+	ATIME=`stat -c %Z /usr/fomtextemplate/deine_inhalte/Kapitel.tex`
 
-   	if [[ "$ATIME" != "$LTIME" ]]
-   	then
-      	texcount /usr/fomtextemplate/deine_inhalte/Kapitel.tex > word_count.log
-    	arara *.tex
-    	LTIME=$ATIME
-    	echo "Word Count: "
-    	texcount /usr/fomtextemplate/deine_inhalte/Kapitel.tex -quiet -brief
-   	fi
-   	# sleep 2
+	if [[ "$ATIME" != "$LTIME" ]]
+	then
+		texcount /usr/fomtextemplate/deine_inhalte/Kapitel.tex > word_count.log
+		arara *.tex
+		LTIME=$ATIME
+		echo "Word Count: "
+		texcount /usr/fomtextemplate/deine_inhalte/Kapitel.tex -quiet -brief
+	fi
+	# sleep 2
 done
